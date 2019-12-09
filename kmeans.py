382146ac3c9af24737b98f82d28344407834f366
Shivam Shah <shivshah12698@gmail.com>
2019-12-09 09:43:39 -0800

    Fixed the code here and there doesn't plot

--- kmeans.py
+++ kmeans.py
@@ -4,13 +4,18 @@ Created on Thu Nov 14 00:45:00 2019
 
 @author: shlakhanpal
 """
-
+import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+#import pandas as pd
+#import h5py
 
-X = pd.read_csv("kmeansdata.mat") # class activity
 
+test = scipy.io.loadmat('kmeansdata.mat')
+
+X = np.array(test['X'])
+#X = h5py.File('kmeansdata.mat', 'r') # class activity
+#X = np.array(Xa)
 print('Printing data in X...')
 print(X)
 
@@ -19,9 +24,9 @@ print('Dimensions of X', X.shape)
 num_points = len(X)
 print("Total number of points in dataset, ie. X:", num_points)
 
-K = 2 # class activity
+K = 3 # class activity
 
-initial_centroids = np.array([[1,1],[2,1]]) # class activity
+initial_centroids = X # class activity
 
 plt.plot(X[:, 0], X[:, 1], 'go')
 plt.plot(initial_centroids[:, 0], initial_centroids[:, 1], 'rx')
